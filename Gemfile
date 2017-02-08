8b304e62ef67ba8d6c70faa02ca7121ea9c1d2e6
masahiro aizawa <kingyodojo@gmail.com>
2017-02-08 23:46:20 +0900

    ざっくり

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'octoparts'
+gem 'multi_json'
+gem 'representable', '2.4.0'
+
+gem 'rspec'
+
---
+++ spec/test_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'json'
+
+describe Octoparts do
+  let(:client) { Octoparts::Client.new }
+  let(:response) { double(:response, status: 200, body: body, headers: []) }
+  let(:body) { '{"result": "ok"}' }
+
+  it do
+    expect(client).to be_instance_of(Octoparts::Client)
+  end
+
+  it do
+    allow_any_instance_of(Octoparts::Client).to receive(:post).with(
+      Octoparts::Client::OCTOPARTS_API_ENDPOINT_PATH,
+      JSON.dump(
+        requests: [{
+          part_id: '',
+          params: []
+        }]
+      ),
+      content_type: 'application/json'
+    ).and_return(response)
+    result = client.invoke(
+      requests: [{
+        part_id: '',
+      }]
+    )
+    expect(result.body).to eq('ok')
+  end
+end
